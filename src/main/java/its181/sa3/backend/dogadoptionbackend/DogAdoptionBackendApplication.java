584e777c1c3f082cb316449c04eec3c2e0af040a
Cedeli <96904745+Cedeli@users.noreply.github.com>
2025-03-16 16:00:18 +0800

    Initial commit

---
+++ src/main/java/its181/sa3/backend/dogadoptionbackend/DogAdoptionBackendApplication.java
@@ -0,0 +1,13 @@
+package its181.sa3.backend.dogadoptionbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DogAdoptionBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DogAdoptionBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/its181/sa3/backend/dogadoptionbackend/DogAdoptionBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package its181.sa3.backend.dogadoptionbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DogAdoptionBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
